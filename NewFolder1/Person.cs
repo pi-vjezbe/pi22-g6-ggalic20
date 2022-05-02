7a92b366a9adac44e64b2dc5116e1643d861cb45
Programsko Inzenjerstvo <pi_foi@outlook.com>
2022-05-02 20:10:27 +0200

    Implementacija hijerarhija

---
+++ NewFolder1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.NewFolder1
+{
+    public abstract class Person : Object
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ NewFolder1/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.NewFolder1
+{
+    public class Teacher : Person
+    {
+        public class Teacher : Person
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+
+    }
+}
